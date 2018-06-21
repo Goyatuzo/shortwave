ca366cf3fc7e761b2c43ae6b333f8ffee1062308
Yuto Otaguro <yotagur@gmail.com>
2018-06-21 17:55:25 -0500

    Create admin index page.

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,21 @@ const styles = require('./sass/main.scss');
 
 import { render } from 'react-dom';
 import * as React from 'react';
+import createHistory from 'history/createBrowserHistory';
 
 import { GalleryComponent } from './gallery';
+import { Router, Route } from 'react-router';
+import { AdminComponent } from './admin';
+
+const history = createHistory();
 
 render(<GalleryComponent />, document.getElementById("sharkwave-entry"));
+
+
+// render(<Router history={history}>
+//     <React.Fragment>
+//         <Route path="/" component={GalleryComponent}>
+//             <Route path="admin" component={AdminComponent} />
+//         </Route>
+//     </React.Fragment>
+// </Router>, document.getElementById("sharkwave-entry"));
