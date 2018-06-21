fc54be8a389ce253551def9dc0726b2af5d1d84a
Yuto Otaguro <yotagur@gmail.com>
2018-06-21 18:55:38 -0500

    VIdeos can be added via admin.

--- src/admin/index.tsx
+++ src/admin/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import firebase from '../firebase/firebase';
+import { AddGalleryComponent } from './components/add-gallery';
 
 interface ExternalProps {
 
@@ -14,21 +15,21 @@ export class AdminComponent extends React.Component<AdminProps, any> {
     }
 
     inputEl;
-    
-    componentWillMount(){
+
+    componentWillMount() {
         /* Create reference to messages in Firebase Database */
         let messagesRef = firebase.database().ref('messages').orderByKey().limitToLast(100);
         messagesRef.on('child_added', snapshot => {
             /* Update React state when message is added at Firebase Database */
             let message = { text: snapshot.val(), id: snapshot.key };
             this.setState({ messages: [message].concat(this.state.messages) });
-        })
+        });
     }
 
-    addMessage(e){
+    addMessage(e) {
         e.preventDefault(); // <- prevent form submit from reloading the page
         /* Send the message to Firebase */
-        firebase.database().ref('messages').push( this.inputEl.value );
+        firebase.database().ref('messages').push(this.inputEl.value);
         this.inputEl.value = ''; // <- clear the input
     }
 
@@ -39,15 +40,7 @@ export class AdminComponent extends React.Component<AdminProps, any> {
                     Hello Admin!
                 </div>
 
-                <form onSubmit={this.addMessage.bind(this)}>
-                    <input type="text" ref={ el => this.inputEl = el }/>
-                    <input type="submit"/>
-                    <ul>
-                    { /* Render the list of messages */
-                        this.state.messages.map( message => <li key={message.id}>{message.text}</li> )
-                    }
-                    </ul>
-                </form>
+                <AddGalleryComponent />
             </div>
         )
     }
