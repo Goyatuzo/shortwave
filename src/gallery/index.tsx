71cb464749ba24b2fc8fd1bdd9eba749cf47211b
Nicholas Carnevale <ncarnevale@adagetech.com>
2018-06-21 18:33:35 -0500

    fix images

--- src/gallery/index.tsx
+++ src/gallery/index.tsx
@@ -18,7 +18,7 @@ export class GalleryComponent extends React.Component<GalleryProps, null> {
                 <p className="hello">Hello World!!</p>
                 <VideoThumbnailComponent
                     onClick = {(e) => console.log('click!')}
-                    imgSrc={"../imgs/nick.jpg"}
+                    imgSrc={"../src/imgs/shark.jpg"}
                 />
             </div>
         );
