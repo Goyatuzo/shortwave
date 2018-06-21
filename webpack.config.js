4eaee10d7d70ee6164ebeec6750cc87564c14c84
Yuto Otaguro <yotagur@gmail.com>
2018-06-21 16:46:05 -0500

    Add configuration files.

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+/// <binding AfterBuild='Run - Development' />
+
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = [
+    {
+        entry: {
+            global: './src/champion-damage/index'
+        },
+        output: {
+            filename: 'champion-damage.js',
+            path: __dirname + '/dist/'
+        },
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js']
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    loader: 'ts-loader',
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        // fallback to style-loader in development
+                        MiniCssExtractPlugin.loader,
+                        "css-loader",
+                        "sass-loader"
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new webpack.ProvidePlugin({
+                Promise: 'es6-promise'
+            }),
+            
+            new MiniCssExtractPlugin({
+                // Options similar to the same options in webpackOptions.output
+                // both options are optional
+                filename: "main.css",
+                chunkFilename: "[id].css"
+            }),
+
+            new HtmlWebpackPlugin({
+                hash: true,
+                template: './src/index.html',
+                filename: 'index.html'
+            })
+        ]
+    }
+]
