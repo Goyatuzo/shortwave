452e4fff209d4542fe5b348452997e6e056dc209
Jorge Luna <jluna@adagetech.com>
2018-06-22 10:56:26 -0500

    Initial admin form markup

--- src/admin/index.tsx
+++ src/admin/index.tsx
@@ -15,32 +15,9 @@ export class AdminComponent extends React.Component<AdminProps, any> {
         this.state = { messages: [] }; // <- set up react state
     }
 
-    inputEl;
-
-    componentWillMount() {
-        /* Create reference to messages in Firebase Database */
-        let messagesRef = firebase.database().ref('messages').orderByKey().limitToLast(100);
-        messagesRef.on('child_added', snapshot => {
-            /* Update React state when message is added at Firebase Database */
-            let message = { text: snapshot.val(), id: snapshot.key };
-            this.setState({ messages: [message].concat(this.state.messages) });
-        });
-    }
-
-    addMessage(e) {
-        e.preventDefault(); // <- prevent form submit from reloading the page
-        /* Send the message to Firebase */
-        firebase.database().ref('messages').push(this.inputEl.value);
-        this.inputEl.value = ''; // <- clear the input
-    }
-
     render() {
         return (
             <div>
-                <div>
-                    Hello Admin!
-                </div>
-
                 <AddGalleryComponent />
                 <AdminListingComponent />
             </div>
