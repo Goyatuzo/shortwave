cf895b039f6ef04853477c04338bf477292a7faf
Yuto Otaguro <yotagur@gmail.com>
2018-06-22 15:55:35 -0500

    Test SVG

--- src/gallery/video-thumbnail.tsx
+++ src/gallery/video-thumbnail.tsx
@@ -10,5 +10,5 @@ interface VideoThumbnailProps {
 
 export const VideoThumbnailComponent: React.SFC<VideoThumbnailProps> = (props: VideoThumbnailProps) => {
     const iconSrc = "../src/imgs/VIDEO.svg";
-    return <ThumbnailComponent {...props} iconSrc={iconSrc} />;
+    return <ThumbnailComponent {...props} iconSrc="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNDAiIGhlaWdodD0iNDAiIHZpZXdCb3g9IjAgMCA0MCA0MCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48Y2lyY2xlIGN4PSIyMCIgY3k9IjIwIiByPSIyMCIgZmlsbD0id2hpdGUiLz48cGF0aCBkPSJNMCAxMS4zNjM2VjBMMTAuOTA5MSA1LjkwOTA5TDAgMTEuMzYzNloiIHRyYW5zZm9ybT0idHJhbnNsYXRlKDE2LjM2MzYgMTQuNTQ1NSkiIGZpbGw9ImJsYWNrIi8+PC9zdmc+" />;
 }
